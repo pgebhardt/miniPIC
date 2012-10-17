dc297c5ab5228c1fac1fd2dcb4af53fe85696465
Patrik Gebhardt <patrik.gebhardt@rub.de>
2012-10-17 16:56:22 +0200

    minimal PIC seams to work

--- miniPIC.py
+++ miniPIC.py
@@ -1,52 +1,65 @@
 from pylab import *
+from numpy.random import normal
 from scipy import constants
 from scipy.linalg import toeplitz
+from scipy.signal import convolve
 from scipy.interpolate import interp1d
-import time
-
-class Timer():
-    def __enter__(self):
-        self.start = time.time()
-
-    def __exit__(self, *args):
-        print 1e3 * (time.time() - self.start)
 
 def main():
     # parameter
-    length, particleCount, superparticle, dt, dx = 0.1, 1e6, 1e3, 1e-9, 1e-4
-    grid, gridMids, voltage = linspace(0.0, length, length / dx + 1), linspace(0.5 * dx, length - 0.5 * dx, length / dx), array([1.0, 0.0])
-    mass = constants.electron_mass
+    length, dt, dx, electrodeArea = 0.05, 5e-11, 0.05 / 500, 2.0 * pi * sqrt(0.05)
+    grid, gridMids = linspace(0.0, length, length / dx + 1), linspace(0.5 * dx, length - 0.5 * dx, length / dx)
+    particleCount, particleWeight = 1e5, 2e9
 
     # init particles
     electrons, ions = zeros((particleCount, 2)), zeros((particleCount, 2))
-    electrons[:, 0], ions[:, 0] = ogrid[0.0:length:particleCount * 1j], ogrid[0.0:length:particleCount * 1j]
+    electrons[:, 0], ions[:, 0] = normal(length / 2.0, length / 8.0, particleCount), normal(length / 2.0, length / 8.0, particleCount)
+    electrons[:, 1], ions[:, 1] = normal(0.0, 1000.0, particleCount), normal(0.0, 100.0, particleCount)
+    chargeWeight = constants.elementary_charge * particleWeight / (dx * electrodeArea)
 
     # init poisson solver
+    voltage = array([250.0, 0.0])
     phi, roh = zeros((length / dx + 1, )), zeros((length / dx - 1, ))
-    poisson = inv(toeplitz([2.0, -1.0] + [0.0] * (phi.shape[0] - 4)) / dx ** 2) * constants.epsilon_0
+    poisson = inv(toeplitz([2.0, -1.0] + [0.0] * (phi.shape[0] - 4))) * dx ** 2 / constants.epsilon_0
 
     # pic cycle
-    for i in range(1):
+    for i in range(10000):
         # calc roh
-        print 'calc roh:'
-        with Timer():
-            roh = superparticle * histogram(ions[:, 0], grid)[0] - histogram(electrons[:, 0], grid)[0]
+        roh = chargeWeight * (histogram(ions[:, 0], grid)[0] - histogram(electrons[:, 0], grid)[0])
+        for j in range(5):
+            roh = convolve(roh, ones((20, )) / 20.0, 'same')
 
         # calc field
-        print 'calc field:'
-        with Timer():
-            phi[0], phi[1:-1], phi[-1] = voltage[0], dot(poisson, 0.5 * (roh[:-1] + roh[1:])) + grid[1:-1] * (voltage[1] - voltage[0]) / length + voltage[0], voltage[1]
-            eField = interp1d(gridMids, (phi[1:] - phi[:-1]) / dx, bounds_error=False, fill_value=0.0)
+        phi[0], phi[1:-1], phi[-1] = voltage[0], dot(poisson, 0.5 * (roh[:-1] + roh[1:])) + grid[1:-1] * (voltage[1] - voltage[0]) / length + voltage[0], voltage[1]
+        eField = interp1d(gridMids, -(phi[1:] - phi[:-1]) / dx, bounds_error=False, fill_value=0.0)
 
         # move particles
-        print 'move particles:'
-        with Timer():
-            electrons[:, 1] -= eField(electrons[:, 0]) * dt * (constants.elementary_charge / mass)
-            electrons[:, 0] += electrons[:, 1] * dt
-            ions[:, 1] += eField(ions[:, 0]) * dt * (constants.elementary_charge / mass)
-            ions[:, 0] += ions[:, 1] * dt
+        electrons[:, 1] -= eField(electrons[:, 0]) * dt * constants.elementary_charge / constants.electron_mass
+        electrons[:, 0] += electrons[:, 1] * dt
+        ions[:, 1] += eField(ions[:, 0]) * dt * constants.elementary_charge / (constants.neutron_mass * 16)
+        ions[:, 0] += ions[:, 1] * dt
 
-    plot(histogram(electrons[:, 1], grid)[0])
+        # print step
+        if i % 100 == 0:
+            print 'step: {}'.format(i)
+
+    # plot electic fiel
+    subplot(221)
+    plot(eField(gridMids))
+
+    # plot potential
+    subplot(222)
+    plot(phi)
+
+    # plot charge density
+    subplot(223)
+    plot(roh)
+
+    # plot particle densit
+    subplot(224)
+    plot(histogram(ions[:, 0], grid)[0])
+    plot(histogram(electrons[:, 0], grid)[0])
+
     show()
 
 if __name__ == '__main__':
