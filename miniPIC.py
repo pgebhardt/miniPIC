e90c94a2bb647070580e5ed7cc1931309ad45f5f
Patrik Gebhardt <patrik.gebhardt@rub.de>
2012-10-17 11:41:49 +0200

    field calculation works

--- miniPIC.py
+++ miniPIC.py
@@ -1,22 +1,26 @@
 from pylab import *
 from scipy import constants
 from scipy.linalg import toeplitz
+from scipy.interpolate import interp1d
 
 def main():
     # parameter
-    length = 0.1
+    length, particleCount, dt, dx = 0.1, 1e6, 1e-9, 1e-4
+    grid, voltage = linspace(0.0, length, length / dx + 1), zeros((2, ))
 
-    # delta values
-    dt, dx = 1e-9, 1e-4
-
-    # init arrays
-    phi, eField = zeros((length / dx + 1, )), zeros((length / dx + 1, ))
+    # init particles
+    electrones, ions = zeros((particleCount, 2)), zeros((particleCount, 2))
+    electrones[:, 0], ions[:, 0] = ogrid[0.0:length:particleCount * 1j], ogrid[0.0:length:particleCount * 1j]
 
     # init poisson solver
-    poissonMatrix = inv(toeplitz([2.0, -1.0] + [0.0] * (phi.shape[0] - 4)) / dx ** 2) * constants.epsilon_0
+    phi, roh = zeros((length / dx + 1, )), histogram(ions[:, 0], int(length / dx))[0] - histogram(electrones[:, 0], int(length / dx))[0]
+    poisson = inv(toeplitz([2.0, -1.0] + [0.0] * (phi.shape[0] - 4)) / dx ** 2) * constants.epsilon_0
 
-    # solve
-    phi[1:-1] = dot(poissonMatrix, ones(phi.shape)[1:-1])
+    # pic cycle
+    for i in range(10):
+        # calc field
+        phi[1:-1] = dot(poisson, 0.5 * (roh[:-1] + roh[1:])) + grid[1:-1] * (voltage[1] - voltage[0]) / length + voltage[0]
+        eField = interp1d(0.5 * (grid[:-1] + grid[:-1]), (phi[1:] - phi[:-1]) / dx)
 
     plot(phi)
     show()
